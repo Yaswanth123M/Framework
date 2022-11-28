abb39763be06c7a8ad6210e5b9e25a1012f60f25
Yaswanth123M <yaswanthmavilla@gmail.com>
2022-11-28 17:56:41 +0530

    yash

---
+++ src/test/java/com/cts/Test1.java
@@ -0,0 +1,20 @@
+package com.cts;
+
+public class Test1 {
+	public static void main(String[] args) {
+		int a=10, b=20, c=0;
+		System.out.println("Before swapping a:" +a);
+		System.out.println("Before swapping b:" +b);
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("After swapping a:" +a);
+		System.out.println("After swapping b:" +b);
+		
+		
+	}
+	
+}	
+	
